381e2e5bed87ab7b57ee56794ce6123a96f9caa1
Tomer Laor <tomer.laor1@gmail.com>
2021-07-17 16:06:00 +0300

    comment

--- evaluate_fix_match.bash
+++ evaluate_fix_match.bash
@@ -4,5 +4,5 @@
 # To evaluate FixMatch:
 python run_kfold_search.py DATASET.label_num=10 DATASET.strongaugment='RA' EXPERIMENT.epoch_n=1
 
-# To evaluate our suggested improvement:
+# To evaluate our suggested improvement (Ratio Fix Match):
 python run_kfold_search.py DATASET.label_num=10 DATASET.strongaugment='RA' EXPERIMENT.epoch_n=1 EXPERIMENT.is_suggested_improvement=1
