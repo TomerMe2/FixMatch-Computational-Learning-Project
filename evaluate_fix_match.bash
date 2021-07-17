ad7064b39749f8daa0f13ecc9ef973a55ce6c637
Tomer Laor <tomer.laor1@gmail.com>
2021-07-17 15:48:40 +0300

    added bash script to evaluate

---
+++ evaluate_fix_match.bash
@@ -0,0 +1,8 @@
+# This file contains the way that we used to evaluate FixMatch, our suggested improvement and the other well known algorithm.
+# We used 1 epoch since we didn't have the computing power to do more epochs in feasible time
+
+# To evaluate FixMatch:
+python run_kfold_search.py DATASET.label_num=10 DATASET.strongaugment='RA' EXPERIMENT.epoch_n=1
+
+# To evaluate our suggested improvement:
+python run_kfold_search.py DATASET.label_num=10 DATASET.strongaugment='RA' EXPERIMENT.epoch_n=1 EXPERIMENT.is_suggested_improvement=1
